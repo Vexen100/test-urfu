03615831b610459963eec925478bf0cb8de026e8
Vexen100 <van_ole@mail.ru>
2025-10-06 13:09:17 +0500

    Add files via upload

---
+++ cakes.py
@@ -0,0 +1,12 @@
+def print_pack_report(cakes_number):
+    for num in range(cakes_number, 0, -1):
+        if num % 3 == num % 5 == 0:
+            print(f'{num} - расфасуем по 3 или по 5')
+        elif num % 3 == 0 and num % 5 != 0:
+            print(f'{num} - расфасуем по 3')
+        elif num % 3 != 0 and num % 5 == 0:
+            print(f'{num} - расфасуем по 5')
+        else:
+            print(f'{num} - не заказываем!')
+
+print_pack_report(100)
